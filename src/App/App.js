9f24bacb8c11e193645c8e8ff12047c947a50605
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-12-07 13:46:22 -0600

    setup complete

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className='btn btn-info'>SPORTS ROSTER</button>
         <a
           className="App-link"
           href="https://reactjs.org"
