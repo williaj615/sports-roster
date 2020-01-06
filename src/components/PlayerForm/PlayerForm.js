f0e736d6451dc502ab7b0d644062c96501b64979
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-01-05 21:57:08 -0600

    WIP, new players can be created

---
+++ src/components/PlayerForm/PlayerForm.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import authData from '../../helpers/data/authData';
+
+class PlayerForm extends React.Component {
+  static propTypes = {
+    addPlayer: PropTypes.func,
+  }
+
+  state = {
+    playerName: '',
+    playerImageUrl: '',
+    playerPosition: '',
+
+  }
+
+  savePlayerEvent = (e) => {
+    const { addPlayer } = this.props;
+
+    e.preventDefault();
+    const newPlayer = {
+      name: this.state.playerName,
+      imageUrl: this.state.playerImageUrl,
+      position: this.state.playerPosition,
+      uid: authData.getUid(),
+    };
+    addPlayer(newPlayer);
+    this.setState({ playerName: '', playerImageUrl: '', playerPosition: '' });
+  }
+
+  nameChange = (e) => {
+    e.preventDefault();
+    this.setState({ playerName: e.target.value });
+  }
+
+  imageUrlChange = (e) => {
+    e.preventDefault();
+    this.setState({ playerImageUrl: e.target.value });
+  }
+
+  positionChange = (e) => {
+    e.preventDefault();
+    this.setState({ playerPosition: e.target.value });
+  }
+
+  render() {
+    return (
+      <form className='col-6 offset-3 PlayerForm'>
+      <div className="form-group">
+        <label htmlFor="player-name">Player Name:</label>
+        <input
+          type="text"
+          className="form-control"
+          id="player-name"
+          placeholder="Enter player name"
+          value={this.state.playerName}
+          onChange={this.nameChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="player-position">Player Position:</label>
+        <input
+          type="text"
+          className="form-control"
+          id="player-position"
+          placeholder="Enter player position"
+          value={this.state.playerPosition}
+          onChange={this.positionChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="player-image">Image URL:</label>
+        <input
+          type="text"
+          className="form-control"
+          id="player-image"
+          placeholder="Enter player image url"
+          value={this.state.playerImageUrl}
+          onChange={this.Change}
+        />
+      </div>
+      <button className="btn btn-secondary" onClick={this.savePlayerEvent}>Save Player</button>
+    </form>
+    );
+  }
+}
+
+export default PlayerForm;
--- src/components/PlayersContainer/PlayersContainer.js
+++ src/components/PlayersContainer/PlayersContainer.js
@@ -3,6 +3,7 @@ import authData from '../../helpers/data/authData';
 import playerData from '../../helpers/data/playerData';
 import './PlayersContainer.scss';
 import Player from '../Player/Player';
+import PlayerForm from '../PlayerForm/PlayerForm';
 
 class PlayersContainer extends React.Component {
 state = {
@@ -25,14 +26,25 @@ removeSinglePlayer = (playerId) => {
     .catch((errOnRemovePlayer) => console.error(errOnRemovePlayer));
 }
 
+addAPlayer = (newPlayer) => {
+  playerData.savePlayer(newPlayer)
+    .then(() => {
+      this.getPlayers();
+    })
+    .catch((errOnSavePlayer) => console.error(errOnSavePlayer));
+}
+
 componentDidMount() {
   this.getPlayers();
 }
 
 render() {
   return (
-    <div id="players-container" className="d-flex flex-row flex-wrap justify-content-around">
-      {this.state.players.map((player) => (<Player key={player.id} player={player} removeSinglePlayer={this.removeSinglePlayer}/>))}
+    <div>
+      <PlayerForm addPlayer={this.addAPlayer} />
+      <div id="players-container" className="d-flex flex-row flex-wrap justify-content-around">
+        {this.state.players.map((player) => (<Player key={player.id} player={player} removeSinglePlayer={this.removeSinglePlayer}/>))}
+      </div>
     </div>
   );
 }
